792460efc2f13aa8626ca33bc7d71ff38761802a
Arnav Jain <arnavj22@gmail.com>
2022-03-29 14:03:10 -0400

    recommit

---
+++ Lab08/Part1/l081.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace cv;
+using namespace std;
+void part1(){
+    
+}
+int main(){
+    part1()
+}
