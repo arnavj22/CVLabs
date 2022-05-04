365aad0ca5a98da046aa149256f4d175d6264a02
Arnav Jain <arnavj22@gmail.com>
2022-05-04 17:25:12 -0400

    8.2

--- Lab08/Part2/l082.cpp
+++ Lab08/Part2/l082.cpp
@@ -33,6 +33,72 @@ vector<Mat> rotate(vector<Mat> a, int deg){
     return output;
 
 }
+Mat perspective_projection_cube(vector<Mat> a){
+    Mat image(600, 800, CV_8UC3, Scalar(255, 255, 255));
+
+    Mat transformtocenter = (Mat_<double>(4, 4) << 1, 0, 0, 400, 0, 1, 0, 300, 0, 0, 1, 0, 0, 0, 0, 1);
+    vector<Point> points;
+    for (int i = 0; i < a.size(); i++)
+    {
+        Mat temp = a[i];
+        int eye = 1200, plane = 500;
+        double t = (plane - eye) / (temp.at<double>(2, 0) - eye);
+
+        int x = temp.at<double>(0, 0) * t + 400;
+        int y = temp.at<double>(1, 0) * t + 300;
+        points.push_back(Point(x, y));
+        //cout << Point(x, y) << endl;
+        if (x > 0 && x < 800 && y > 0 && y < 600)
+        {
+
+            circle(image, Point(x, y), 2, Scalar(0, 0, 0), 3);
+            // image.at<Vec3b>(x, y) = Vec3b(0, 0, 0);
+        }
+    }
+    line(image, points[0], points[2], Scalar(0, 0, 0));
+    line(image, points[0], points[4], Scalar(0, 0, 0));
+    line(image, points[0], points[1], Scalar(0, 0, 0));
+    line(image, points[1], points[3], Scalar(0, 0, 0));
+    line(image, points[1], points[5], Scalar(0, 0, 0));
+    line(image, points[2], points[6], Scalar(0, 0, 0));
+    line(image, points[2], points[3], Scalar(0, 0, 0));
+    line(image, points[4], points[5], Scalar(0, 0, 0));
+    line(image, points[4], points[6], Scalar(0, 0, 0));
+    line(image, points[7], points[5], Scalar(0, 0, 0));
+    line(image, points[7], points[6], Scalar(0, 0, 0));
+    line(image, points[7], points[3], Scalar(0, 0, 0));
+
+    return image;
+}
+Mat perspective_projection_tetra(vector<Mat> a)
+{
+    Mat image(600, 800, CV_8UC3, Scalar(255, 255, 255));
+
+   // Mat transformtocenter = (Mat_<double>(4, 4) << 1, 0, 0, 400, 0, 1, 0, 300, 0, 0, 1, 0, 0, 0, 0, 1);
+    vector<Point> points;
+    for (int i = 0; i < a.size(); i++)
+    {
+        Mat temp = a[i];
+        int eye = 1200, plane = 500;
+        double t = (plane - eye) / (temp.at<double>(2, 0) - eye);
+        int x = temp.at<double>(0, 0) * t + 400;
+        int y = temp.at<double>(1, 0) * t + 300;
+        points.push_back(Point(x, y));
+        if (x > 0 && x < 800 && y > 0 && y < 600)
+        {
+            circle(image, Point(x, y), 2, Scalar(0, 0, 0), 3);
+            // image.at<Vec3b>(x, y) = Vec3b(0, 0, 0);
+        }
+    }
+    line(image, points[0], points[1], Scalar(0, 0, 0));
+    line(image, points[0], points[2], Scalar(0, 0, 0));
+    line(image, points[0], points[3], Scalar(0, 0, 0));
+    line(image, points[1], points[2], Scalar(0, 0, 0));
+    line(image, points[1], points[3], Scalar(0, 0, 0));
+    line(image, points[2], points[3], Scalar(0, 0, 0));
+
+    return image;
+}
 Mat to2dim(vector<Mat> a){
     Mat image(600, 800, CV_8UC3, Scalar(255, 255, 255));
     
@@ -106,15 +172,15 @@ Mat to2dimtetra(vector<Mat> a)
 
     return image;
 }
-VideoWriter cubeTransform(VideoWriter video){
+VideoWriter cubeTransform(VideoWriter video, bool perspective){
     ofstream myfile;
     myfile.open("coordinates.txt");
     myfile.close();
     Mat image(600, 800, CV_8UC3, Scalar(255, 255, 255));
     Mat scale50 = (Mat_<double>(4, 4) << 100, 0, 0, 0, 0, 100, 0, 0, 0, 0, 100, 0, 0, 0, 0, 1);
     double radian = 30 * M_PI / 180;
-    Mat rotatedown = (Mat_<double>(4, 4) << cos(radian), 0, sin(radian), 0, 0, 1, 0, 0, -sin(radian), 0, cos(radian), 0, 0, 0, 0, 1);
-    Mat transformation = matrixMultiply(rotatedown, scale50);
+    //Mat rotatedown = (Mat_<double>(4, 4) << cos(radian), 0, sin(radian), 0, 0, 1, 0, 0, -sin(radian), 0, cos(radian), 0, 0, 0, 0, 1);
+    Mat transformation = scale50;
     vector<Mat> points; // store a vector of the points +- 1 represented as homogenous matrices
     for (int i = 0; i < 8; i++)
     {
@@ -132,18 +198,24 @@ VideoWriter cubeTransform(VideoWriter video){
     vector<Mat> rotated = points;
     for (int i = 0; i <= 180; i++)
     {
-
-
         if(i > 0 && i < 5){
             coordsToText(rotated);
         }
         rotated = rotate(points, i);
-        Mat image = to2dim(rotated);
+        Mat image;
+        if(perspective){
+            image = perspective_projection_cube(rotated);
+        }
+        else{
+            image = to2dim(rotated);
+        }
+
+        
         video.write(image);
     }
     return video;
 }
-VideoWriter tetraTransform(VideoWriter video)
+VideoWriter tetraTransform(VideoWriter video, bool perspective)
 {
     Mat image(600, 800, CV_8UC3, Scalar(255, 255, 255));
     Mat transformation = (Mat_<double>(4, 4) << 100, 0, 0, 0, 0, 100, 0, 0, 0, 0, 100, 0, 0, 0, 0, 1);
@@ -171,16 +243,29 @@ VideoWriter tetraTransform(VideoWriter video)
             coordsToText(rotated);
         }
         rotated = rotate(points, i);
-        Mat image = to2dimtetra(rotated);
+        if (perspective)
+        {
+            image = perspective_projection_tetra(rotated);
+        }
+        else
+        {
+            image = to2dimtetra(rotated);
+        }
         video.write(image);
     }
     return video;
 }
 void part1(){
     VideoWriter video("rotation.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, Size(800, 600), true);
-    video = cubeTransform(video);
-    video = tetraTransform(video);
+    video = cubeTransform(video, false);
+    video = tetraTransform(video, false);
+}
+void part2(){
+    VideoWriter video("rotation.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, Size(800, 600), true);
+    video = cubeTransform(video, true);
+    video = tetraTransform(video, true);
 }
 int main(){
-    part1();
+    //part1();
+    part2();
 }
